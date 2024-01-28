d014be78c53a18fd4e029a110de9f95a513420df
zidanhafiz <nadizsonata@gmail.com>
2024-01-28 10:28:12 +0800

    Add fonts module

---
+++ utils/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Limelight } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const limelight = Limelight({ subsets: ['latin'], weight: '400' });
