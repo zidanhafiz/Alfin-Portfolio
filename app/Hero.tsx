4f8e32e71791b470d01b51612a40127dd17ae296
zidanhafiz <nadizsonata@gmail.com>
2024-01-28 10:43:05 +0800

    Add semantic html

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -1,27 +1,22 @@
 'use client';
-import Navbar from '@/components/Navbar';
+import { inter, limelight } from '@/utils/fonts';
 import clsx from 'clsx';
-import { Limelight } from 'next/font/google';
 import { motion } from 'framer-motion';
-
-const limelight = Limelight({ subsets: ['latin'], weight: '400' });
 
 const Hero = () => {
   return (
-    <div
-      id='home'
-      className='bg-gradient-to-r from-black via-gray-900 to-gray-800 py-8 md:py-12 h-[700px]'
-    >
-      <Navbar />
-      <div className='mt-20 md:mt-52 mx-4 md:mx-28'>
+    <div id='home'>
+      <div className='mx-auto h-screen flex justify-center items-center'>
         <motion.h1
-          className={clsx(limelight.className, 'text-3xl md:text-6xl md:leading-[1.2]')}
+          className={clsx(
+            inter.className,
+            'text-center text-3xl md:text-5xl md:leading-[1.2]'
+          )}
           initial={{ y: 100, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
+          whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 1 }}
         >
-          Capturing
-          <br /> moments with
+          Capturing moments with
           <br /> Alfin Ilham Maulidi.
         </motion.h1>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Navbar from '@/components/Navbar';
 import Hero from './Hero';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Home() {
   return (
     <>
-      <main className='text-white h-[1000px]'>
+      <header className='w-full fixed top-0 py-4 md:py-10'>
+        <Navbar />
+      </header>
+      <main className='bg-gradient-to-r from-black via-gray-900 to-gray-800 min-h-screen'>
+        <Hero />
         <Hero />
       </main>
     </>
