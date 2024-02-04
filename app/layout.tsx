e5cd4bb7e9e930987e2e6e24888f61d181439c70
zidanhafiz <nadizsonata@gmail.com>
2024-02-04 10:39:40 +0800

    Add opengraph to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,22 @@ import { inter } from '@/utils/fonts';
 export const metadata: Metadata = {
   title: 'Alfin Ilham Maulidi | Portfolio',
   description: "Alfin's personal portfolio website.",
+  openGraph: {
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: '/opengraph-image.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
