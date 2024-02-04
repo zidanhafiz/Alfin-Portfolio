1676770b90df45d28485e55f1fd8a242b7b9207a
zidanhafiz <nadizsonata@gmail.com>
2024-02-04 09:43:27 +0800

    Set scroll behavior to smooth

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html
+      lang='en'
+      className='scroll-smooth'
+    >
       <body className={`${inter.className} text-white`}>{children}</body>
     </html>
   );
