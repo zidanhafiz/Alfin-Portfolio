e931d53aadead51a1c8ffd8c5b69a11fbe5b856f
zidanhafiz <nadizsonata@gmail.com>
2024-02-04 09:48:24 +0800

    Remove unused class on main tag, add contact and footer section and move header tag into navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,21 @@ import Hero from './Hero';
 import About from './About';
 import Projects from './Projects';
 import Skills from './Skills';
+import Contact from './Contact';
+import Footer from './Footer';
 
 export default function Home() {
   return (
     <>
-      <header className='w-full fixed top-0 py-4 md:py-10 z-50'>
-        <Navbar />
-      </header>
-      <main className='bg-gradient-to-tr from-black via-black to-slate-900'>
+      <Navbar />
+      <main>
         <Hero />
         <About />
         <Projects />
         <Skills />
+        <Contact />
       </main>
+      <Footer />
     </>
   );
 }
